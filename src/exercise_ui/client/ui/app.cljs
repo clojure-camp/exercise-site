e07c1b7a6febd9fa8bc98df9a3c54261e3f4def8
Rafal Dittwald <rafal.dittwald@gmail.com>
2019-10-21 16:12:30 -0400

    Add link to home on all pages

--- src/exercise_ui/client/ui/app.cljs
+++ src/exercise_ui/client/ui/app.cljs
@@ -1,8 +1,10 @@
 (ns exercise-ui.client.ui.app
   (:require
+    [bloom.commons.pages :refer [path-for]]
     [re-frame.core :refer [subscribe]]
     [exercise-ui.client.pages :as pages]))
 
 (defn app-view []
   [:div
+   [:a {:href (path-for :home)} "Home"]
    [pages/current-page-view]])
