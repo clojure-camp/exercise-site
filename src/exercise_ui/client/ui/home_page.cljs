c53f84d2dd1a1f8480d280cfff6179f8f252bf95
Rafal Dittwald <rafal.dittwald@gmail.com>
2019-10-21 16:58:03 -0400

    Show "uses" of each exercise

--- src/exercise_ui/client/ui/home_page.cljs
+++ src/exercise_ui/client/ui/home_page.cljs
@@ -1,11 +1,18 @@
 (ns exercise-ui.client.ui.home-page
   (:require
+    [clojure.string :as string]
     [re-frame.core :refer [subscribe]]
     [bloom.commons.pages :refer [path-for]]))
 
 (defn home-page-view [params]
   [:div.page.home
-   (for [exercise @(subscribe [:exercises])]
-     ^{:key (exercise :id)}
-     [:a {:href (path-for :exercise {:exercise-id (exercise :id)})}
-      (exercise :id)])])
+   [:table
+    [:tbody
+     (for [exercise @(subscribe [:exercises])]
+       ^{:key (exercise :id)}
+       [:tr
+        [:td
+         [:a {:href (path-for :exercise {:exercise-id (exercise :id)})}
+          (exercise :id)]]
+        [:td
+         (string/join " " (sort (map str (exercise :uses))))]])]]])
