14c61792310f8949b5fc396d8007fafe2edcb6d8
Rafal Dittwald <rafal.dittwald@gmail.com>
2020-03-26 15:08:33 -0400

    Add login-url literal link to html email

--- src/exercise_ui/server/email.clj
+++ src/exercise_ui/server/email.clj
@@ -18,4 +18,4 @@
             :content (str "Go to this link to log in to the Clojure Workshop site:\n"
                           login-link)}
            {:type "text/html; charset=utf-8"
-            :content (str "<html><body><p>Click <a href=\"" login-link "\">here</a> to log in to the Clojure Workshop site</p></body></html>")}]})))
+            :content (str "<html><body><p>Click <a href=\"" login-link "\">here</a> to log in to the Clojure Workshop site:</p><p><a href=\"" login-link "\">" login-link "</a></p></body></html>")}]})))
