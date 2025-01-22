a11868f5f8cacc96b444ca35fca121338eb496e5
Taulant Sulko <taulant.sulko@gmail.com>
2025-01-22 17:41:23 -0500

    WIP parsing and displaying

--- src/exercise_ui/client/ui/pages/exercise.cljs
+++ src/exercise_ui/client/ui/pages/exercise.cljs
@@ -1,22 +1,23 @@
 (ns exercise-ui.client.ui.pages.exercise
   (:require
-    [clojure.string :as string]
-    [bloom.commons.fontawesome :as fa]
-    [bloom.commons.pages :refer [path-for]]
-    [reagent.core :as r]
-    [re-frame.core :refer [subscribe dispatch]]
-    [exercise-ui.client.ui.styles :as styles]
-    [exercise-ui.utils :refer [parse-backticks]]
-    [exercise-ui.client.ui.partials.code-view :refer [code-view format-code]]
-    [exercise-ui.client.ui.partials.teachable :refer [teachable-view]]
-    [exercise-ui.client.i18n :as i18n]))
+   [clojure.string :as string]
+   [bloom.commons.fontawesome :as fa]
+   [bloom.commons.pages :refer [path-for]]
+   [reagent.core :as r]
+   [re-frame.core :refer [subscribe dispatch]]
+   [exercise-ui.client.ui.styles :as styles]
+   [exercise-ui.utils :refer [parse-backticks]]
+   [exercise-ui.client.ui.partials.code-view :refer [code-view format-code]]
+   [exercise-ui.client.ui.partials.teachable :refer [teachable-view]]
+   [exercise-ui.client.ui.pages.blinded-exercise :as blinded]
+   [exercise-ui.client.i18n :as i18n]))
 
 (defn solution-view [exercise]
   (r/with-let [open? (r/atom false)]
     [:section.solution
      [:header {:on-click (fn []
                            (swap! open? not))}
-      [:h2 (i18n/value {:en-US "example solution" :pt-BR "solução exemplo" })]
+      [:h2 (i18n/value {:en-US "example solution" :pt-BR "solução exemplo"})]
       (if @open?
         [fa/fa-chevron-down-solid]
         [fa/fa-chevron-right-solid])]
@@ -30,15 +31,15 @@
      [:header {:tw "gap-2"}
       [:h2 {:tw "grow"} (i18n/value {:en-US "sample tests" :pt-BR "testes de exemplo"})]
       (doall
-        (for [[mode icon] [[:mode/table [fa/fa-table-solid]]
-                           [:mode/rcf "RCF"]
-                           [:mode/clj "CLJ"]]]
-          ^{:key mode}
-          [:button
-           {:on-click (fn [_] (reset! active-mode mode))
-            :tw [(when (= @active-mode mode)
-                   "bg-white")]}
-           icon]))]
+       (for [[mode icon] [[:mode/table [fa/fa-table-solid]]
+                          [:mode/rcf "RCF"]
+                          [:mode/clj "CLJ"]]]
+         ^{:key mode}
+         [:button
+          {:on-click (fn [_] (reset! active-mode mode))
+           :tw [(when (= @active-mode mode)
+                  "bg-white")]}
+          icon]))]
 
      (case @active-mode
        :mode/table
@@ -75,72 +76,93 @@
         [code-view {:class "code"
                     :pre-formatted? true}
          (str
-           "(ns exercises." (:id exercise) "\n"
-           "  (:require\n"
-           "    [hyperfiddle.rcf :as rcf]))\n\n"
-           "(rcf/enable!)\n\n"
-           (when (seq (:function-template exercise))
-             (str (string/join "\n\n" (:function-template exercise)) "\n\n"))
-           "(rcf/tests\n"
-           (string/join "\n\n"
-                        (for [{:keys [input output]} (exercise :test-cases)]
-                          (str "  " (format-code input) " := \n"
-                               "  "
-                               (if (string? output)
-                                 (pr-str output)
-                                 (string/replace (format-code output)
-                                                 "\n" "\n  ")))))
-           ")")]])]))
+          "(ns exercises." (:id exercise) "\n"
+          "  (:require\n"
+          "    [hyperfiddle.rcf :as rcf]))\n\n"
+          "(rcf/enable!)\n\n"
+          (when (seq (:function-template exercise))
+            (str (string/join "\n\n" (:function-template exercise)) "\n\n"))
+          "(rcf/tests\n"
+          (string/join "\n\n"
+                       (for [{:keys [input output]} (exercise :test-cases)]
+                         (str "  " (format-code input) " := \n"
+                              "  "
+                              (if (string? output)
+                                (pr-str output)
+                                (string/replace (format-code output)
+                                                "\n" "\n  ")))))
+          ")")]])]))
+
+(def exercise-styles
+  {:exercise.style/normal "Normal"
+   :exercise.style/blinded "Blinded"})
+
+(defn normal-exercise-view [exercise]
+  [:<>
+   (when (:function-template exercise)
+     [:section.starter-code
+      [:header
+       [:h2 (i18n/value {:en-US "starter code" :pt-BR "código inicial"})]]
+      [:div.body
+       [code-view {:class "code"}
+        (:function-template exercise)]]])
+
+   (let [fns (->> (concat (map (fn [x] [x :teaches]) (exercise :teaches))
+                          (map (fn [x] [x :uses]) (exercise :uses)))
+                  (filter (fn [[f _]] (symbol? f))))]
+     (when (seq fns)
+       [:section.functions
+        [:header
+         [:h2 (i18n/value {:en-US "related functions" :pt-BR "funções relacionadas"})]]
+        [:div.body
+         (into [:<>]
+               (->> fns
+                    (map (fn [[f category]] [teachable-view f (name category)]))
+                    (interpose " ")))]]))
+
+   (when (seq (exercise :test-cases))
+     [test-case-view exercise])
+
+   [solution-view exercise]])
 
 (defn exercise-page-view [exercise-id]
-  (when-let [exercise @(subscribe [:exercise exercise-id])]
-    [:div.page.exercise
-     [:header
-      [:h1 (i18n/value (exercise :title))]]
+  (r/with-let [exercise-style (r/atom :exercise.style/blinded)]
+    (when-let [exercise @(subscribe [:exercise exercise-id])]
+      [:div.page.exercise
+       [:header
+        [:h1 (i18n/value (exercise :title))]
+        (let [k->s name
+              s->k (zipmap (map k->s (keys exercise-styles))
+                           (keys exercise-styles))]
+          [:select {:value @exercise-style
+                    :on-change (fn [e]
+                                 (reset! exercise-style (s->k (.. e -target -value))))}
+           (for [[es label] exercise-styles]
+             ^{:key es}
+             [:option {:value (k->s es)} label])])]
 
-     [:<>
-      [:section.instructions
-       (into [:<>]
-             (for [node (i18n/value (exercise :instructions))]
-               (if (or (not (string? node))
-                       (and
+       [:section.instructions
+        (into [:<>]
+              (for [node (i18n/value (exercise :instructions))]
+                (if (or (not (string? node))
+                        (and
                          (string/starts-with? node "(")
                          (string/ends-with? node ")")))
-                 [code-view {:class "code"
-                             :fragment? true} node]
-                 (into [:p] (parse-backticks node)))))]
+                  [code-view {:class "code"
+                              :fragment? true} node]
+                  (into [:p] (parse-backticks node)))))]
 
-       (when (:function-template exercise)
-         [:section.starter-code
-          [:header
-           [:h2 (i18n/value {:en-US "starter code" :pt-BR "código inicial"})]]
-          [:div.body
-           [code-view {:class "code"}
-            (:function-template exercise)]]])
+       (case @exercise-style
+         :exercise.style/normal
+         [normal-exercise-view exercise]
+         :exercise.style/blinded
+         [blinded/blinded-exercise-view exercise])
 
-      (let [fns (->> (concat (map (fn [x] [x :teaches]) (exercise :teaches))
-                             (map (fn [x] [x :uses]) (exercise :uses)))
-                     (filter (fn [[f _]] (symbol? f))))]
-        (when (seq fns)
-          [:section.functions
-           [:header
-            [:h2 (i18n/value {:en-US "related functions" :pt-BR "funções relacionadas"})]]
-           [:div.body
-            (into [:<>]
-                  (->> fns
-                       (map (fn [[f category]] [teachable-view f (name category)]))
-                       (interpose " ")))]]))
-
-      (when (seq (exercise :test-cases))
-        [test-case-view exercise])
-
-      [solution-view exercise]
-
-      (when (exercise :related)
-        [:div.related
-         [:h2 (i18n/value {:en-US "See also:" :pt-BR "Veja também:"})]
-         [:div.exercises
-          (for [id (exercise :related)]
-            ^{:key id}
-            [:div.exercise
-             [:a {:href (path-for [:exercise {:exercise-id id}])} id]])]])]]))
+       (when (exercise :related)
+         [:div.related
+          [:h2 (i18n/value {:en-US "See also:" :pt-BR "Veja também:"})]
+          [:div.exercises
+           (for [id (exercise :related)]
+             ^{:key id}
+             [:div.exercise
+              [:a {:href (path-for [:exercise {:exercise-id id}])} id]])]])])))
